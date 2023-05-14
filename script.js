94bc0366409ed3712e0f8153ad55967537631d60
Md. Fahim Hossain Sujon <120704636+FahimTS@users.noreply.github.com>
2023-05-14 14:34:40 +0800

    just typed animation
    just text typed animation

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".type-animated",{
+    strings: ["I'm Fahim Hossain", "I'm a Web Developer", "I'm a Digital Marketer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
